e0ef7c1f51a9d9f7c8eadc7087a86eff09e1534f
Sven <810872900@qq.com>
2012-08-08 16:39:14 +0800

    local cm

---
+++ WaterFall_demo/ImageLoader.h
@@ -0,0 +1,13 @@
+//
+//  ImageLoader.h
+//  WaterFall_demo
+//
+//  Created by goojje goojje on 12-8-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageLoader : NSObject
+
+@end
---
+++ WaterFall_demo/ImageLoader.m
@@ -0,0 +1,13 @@
+//
+//  ImageLoader.m
+//  WaterFall_demo
+//
+//  Created by goojje goojje on 12-8-8.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageLoader.h"
+
+@implementation ImageLoader
+
+@end
